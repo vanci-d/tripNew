82b78caeff05505f7462ee0f422c8a7d827a0aa3
vanci <vancura.david@gmail.com>
2024-11-11 16:58:54 +0100

    pridan js s pridanim formu

--- js/script.js
+++ js/script.js
@@ -1,2 +1,35 @@
-console.log("Ahoj světe");
-git;
+const form = document.querySelector("form");
+const inputTitle = form.querySelector("input[name=title]");
+const inputContent = form.querySelector("input[name=content]");
+const cardContainer = document.querySelector(".cards ul");
+
+function createNewCard(container, title, text) {
+	let newCard = document.createElement("li");
+
+	newCard.innerHTML = `
+		<h3>${title}</h3>
+		<p>${text}</p>
+	`;
+
+	container.appendChild(newCard);
+}
+
+form.addEventListener("submit", (event) => {
+	// pointa formularu je odoslat jeho udaje na dalsie spracovanie
+	// cize podobne ako link, za normalnych okolnosti po odoslani formularu
+	// prehliadac skoci na inu web adresu... ak tomu chces zabranit:
+	event.preventDefault();
+
+	// ak nemame title alebo content, koncime
+	if (!inputTitle.value.trim() || !inputContent.value.trim()) return;
+
+	// vytvor novy card element [ funkcia je v utilities.json ]
+	createNewCard(cardContainer, inputTitle.value, inputContent.value);
+
+	// premaz inputy
+	inputTitle.value = "";
+	inputContent.value = "";
+
+	// nastav sa do title inputu
+	inputTitle.focus();
+});
